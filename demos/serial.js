9ad4825e2e9ca987454dd0e3661e9d3133cbc042
Reilly Grant <reillyg@chromium.org>
2016-03-22 14:31:46 -0700

    Update getConfiguration -> selectConfiguration.

--- demos/serial.js
+++ demos/serial.js
@@ -34,15 +34,11 @@ var serial = {};
     };
 
     return this.device_.open()
-        .then(() => this.device_.getConfiguration()
-            .then(config => {
-              if (config.configurationValue == 1) {
-                return Promise.resolve();
-              } else {
-                return Promise.reject("Need to setConfiguration(1).");
-              }
-            })
-            .catch(error => this.device_.setConfiguration(1)))
+        .then(() => {
+          if (this.device_.configuration === null) {
+            return this.device_.selectConfiguration(1);
+          }
+        })
         .then(() => this.device_.claimInterface(2))
         .then(() => this.device_.controlTransferOut({
             'requestType': 'class',
