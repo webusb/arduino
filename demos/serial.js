5ed2d592740aac7ec7aab0cc7770bed19e2f0c3d
Limor "Ladyada" Fried <limor@ladyada.net>
2019-08-26 13:39:41 -0400

    Support all adafruit boards
    we have dozens :) this will make sure they always work!

--- demos/serial.js
+++ demos/serial.js
@@ -23,9 +23,7 @@ var serial = {};
       { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
       { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
       { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
-      { 'vendorId': 0x239A, 'productId': 0x000E }, // Adafruit ItsyBitsy 32u4
-      { 'vendorId': 0x239A, 'productId': 0x800D }, // Adafruit ItsyBitsy 32u4
-      { 'vendorId': 0x239A, 'productId': 0x801B }, // Adafruit Feather M0 Express
+      { 'vendorId': 0x239A }, // Adafruit Boards!
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
